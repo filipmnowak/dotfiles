b61172839f49e9fd2a79a7aa14ce9e1d4b46435c
Filip M. Nowak <github@temp.oneiroi.net>
2023-12-20 22:58:27 +0100

    added has_words_before

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -82,6 +82,11 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 -- Set up nvim-cmp.
 local cmp = require'cmp'
 
